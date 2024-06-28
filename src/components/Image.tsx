b15b495267a718ed2eb42b488800b4b4e88eda38
seung365 <bdh3659@naver.com>
2024-06-29 00:42:06 +0900

    fix: Image 컴포넌트 수정

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,58 +1,52 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
 
 export interface ImageProps {
   src: string;
   radius?: 'circle' | number;
-  ratio?: 'square' | 'auto' | string;
+  ratio?: 'square' | 'auto' | '16/9';
 }
 
-const getPaddingTop = (ratio: 'square' | string) => {
-  if (ratio === 'square') {
-    return '100%';
+const ratioStyles = (ratio?: string) => {
+  switch (ratio) {
+    case 'square':
+      return `
+        width: 100px; height: 100px
+      `;
+    case '16/9':
+      return `
+        width: 160px; height: 90px
+      `;
+    case 'auto':
+      return `
+        width: auto;
+        height: auto;
+      `;
+    default:
+      return `
+        width: 100%;
+        height: auto;
+      `;
   }
-  const numericRatio = ratio.split('/').map(Number);
-  return `calc(${numericRatio[1]} / ${numericRatio[0]} * 100%)`;
 };
 
-const Wrapper = styled.div<{ ratio: 'square' | 'auto' | string }>`
-  position: relative;
-  width: 100%;
-  ${(props) =>
-    props.ratio !== 'auto'
-      ? css`
-          padding-top: ${getPaddingTop(props.ratio)};
-        `
-      : css`
-          height: auto;
-        `}
-`;
-
-const ImageElement = styled.img<{ radius?: 'circle' | number }>`
-  ${(props) => {
-    if (props.radius === 'circle') {
-      return css`
-        border-radius: 50%;
-      `;
+const StyledImage = styled.img<ImageProps>`
+  ${({ radius }) => {
+    if (radius === 'circle') {
+      return `border-radius: 50%;`;
     }
-    if (typeof props.radius === 'number') {
-      return css`
-        border-radius: ${props.radius}px;
-      `;
+    if (typeof radius === 'number') {
+      return `border-radius: ${radius}px;`;
     }
     return '';
   }}
-  width: 100%;
-  height: auto;
+
+  ${({ ratio }) => ratioStyles(ratio)};
+  object-fit: cover;
 `;
 
-function Image({ src, radius, ratio = 'auto' }: ImageProps) {
-  return (
-    <Wrapper ratio={ratio}>
-      <ImageElement src={src} radius={radius} />
-    </Wrapper>
-  );
+function Image({ src, radius, ratio }: ImageProps) {
+  return <StyledImage src={src} alt="" radius={radius} ratio={ratio} />;
 }
 
 export default Image;
